6d02ef26cd58ce48b1c6cb1bef4340a3f507741f
Katya Ruch <katya.ruch@gmail.com>
2019-10-05 22:42:30 +0300

    Update 2019-10-05T19:42:21.456Z

--- pages/Absolute/js/main.js
+++ pages/Absolute/js/main.js
@@ -8,6 +8,14 @@ $(document).ready(function() {
     // slidesToScroll: 1,
   });
 
+  $('.banks__slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  });
+
   $('.btn-next').on('click',function(){ $('#slick').slick('slickNext'); });
   $('.btn-prev').on('click',function(){ $('#slick').slick('slickPrev'); });
 
@@ -44,6 +52,52 @@ $(document).ready(function() {
       }
     ]
   });
+
+  
+
+    $('.calc__range').rangeslider({
+    // Feature detection the default is `true`.
+      // Set this to `false` if you want to use
+      // the polyfill also in Browsers which support
+      // the native <input type="range"> element.
+      polyfill: false,
+
+      // Default CSS classes
+      rangeClass: 'rangeslider',
+      disabledClass: 'rangeslider--disabled',
+      horizontalClass: 'rangeslider--horizontal',
+      fillClass: 'rangeslider__fill',
+      handleClass: 'rangeslider__handle',
+
+      // Callback function
+      onInit: function() {
+        $rangeEl = this.$range;
+        // add value label to handle
+        var $handle = $rangeEl.find('.rangeslider__handle');
+        var handleValue = '<div class="rangeslider__handle__value">' + this.value + '</div>';
+        $rangeEl.append(handleValue);
+        
+        // get range index labels 
+        var rangeLabels = this.$element.attr('labels');
+        rangeLabels = rangeLabels.split(', ');
+        
+        // add labels
+        $rangeEl.append('<div class="rangeslider__labels"></div>');
+        $(rangeLabels).each(function(index, value) {
+          $rangeEl.find('.rangeslider__labels').append('<span class="rangeslider__labels__label">' + value + '</span>');
+        })
+      },
+
+      // Callback function
+      onSlide: function(position, value) {
+        var $handle = this.$range.find('.rangeslider__handle__value');
+        $handle.text(this.value);
+      },
+
+      // Callback function
+      onSlideEnd: function(position, value) {}
+  });
+
 
   // var myMap;
   // ymaps.ready(init);
