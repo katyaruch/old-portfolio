c2d5cd82a2081998c650eebb947b21d0bb13f0e0
Katya Ruch <katya.ruch@gmail.com>
2019-10-07 18:32:19 +0300

    Update 2019-10-07T15:32:12.067Z

--- pages/Absolute/js/main.js
+++ pages/Absolute/js/main.js
@@ -105,21 +105,21 @@ $(document).ready(function() {
   });
 
 
-  // $('.popup').magnificPopup({
-  //   delegate: 'a',
-  //   type: 'image',
-  //   // closeOnContentClick: false,
-  //   // closeBtnInside: false,
-  //   // mainClass: 'mfp-with-zoom mfp-img-mobile',
-  //   // gallery: {
-  //   //   enabled: true
-  //   // },
-  //   zoom: {
-  //     enabled: true,
-  //     duration: 300, // don't foget to change the duration also in CSS
-  //     opener: function(element) {
-  //       return element.find('img');
-  //     }
-  //   }
-  // });
+  $('.popup').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    closeOnContentClick: false,
+    closeBtnInside: false,
+    mainClass: 'mfp-with-zoom mfp-img-mobile',
+    gallery: {
+      enabled: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300, // don't foget to change the duration also in CSS
+      opener: function(element) {
+        return element.find('img');
+      }
+    }
+  });
 });
