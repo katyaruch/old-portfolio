d643d9fea124e4e7f654082c0838f0a5f6e04e04
Katya Ruch <katya.ruch@gmail.com>
2019-10-25 17:00:39 +0300

    Update 2019-10-25T14:00:16.787Z

--- pages/mmbusiness/js/main.js
+++ pages/mmbusiness/js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-  
-}
+  //////// burger navigation /////////
+
+  var burgerToggle = document.querySelector(".header__burger");
+  var navigation = document.querySelector(".header__navigation");
+
+  $(burgerToggle).on("click", function (evt) {
+    evt.preventDefault();
+    $(navigation).toggle("header__navigation_show"); //добавляем класс появления меню
+  });
+
+});
