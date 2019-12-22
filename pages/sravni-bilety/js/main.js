4d15d3d246b45fe9774039d1fb0b78e3918d2f02
Katya Ruch <katya.ruch@gmail.com>
2019-12-22 10:57:02 +0300

    Update 2019-12-22T07:56:56.329Z

--- pages/sravni-bilety/js/main.js
+++ pages/sravni-bilety/js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-  
-}
+  // https://bootstrap-datepicker.readthedocs.io/en/latest/index.html
+  $('#date').datepicker({
+    format: "dd M yyyy",
+    language: "ru",
+    todayHighlight: true,    
+    autoclose: true,
+    startDate: "0d",
+    endDate: "+1m",
+    startView: "days",
+    orientation: "bottom left",
+    todayBtn: true,
+    todayHighlight: true,
+    leftArrow: "<i class='fas fa-chevron-left'></i>",
+    rightArrow: "<i class='fas fa-chevron-right'></i>"
+  });
+
+
+});
