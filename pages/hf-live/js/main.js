c413ed6193ed9992b988bc1e6fda5af29c1bf0c7
Katya Ruch <katya.ruch@gmail.com>
2020-03-02 18:09:27 +0300

    Update 2020-03-02T15:09:15.766Z

--- pages/hf-live/js/main.js
+++ pages/hf-live/js/main.js
@@ -1,9 +1,34 @@
 $(document).ready(function(){
   console.log("Hello!");
 
+  //////////// animate scroll
+
+  var buttonPortfolio = document.querySelector(".button-portfolio");
+  var links = document.querySelector(".navigation");
+
+  $('a[href*="#"]').click(function (evt) {
+    evt.preventDefault();
+
+    var id  = $(this).attr('href'),
+      top = $(id).offset().top - 60;
+    $('body,html').animate({scrollTop: top}, 1500);
+  });
+
+
+  //////////// slick
+
   $('.reviews-list').slick({
     infinite: false,
     slidesToShow: 2,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 })
