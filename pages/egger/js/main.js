1b3ea518caf0d0b06d937bbc11f10ac4cc747702
Katya Ruch <katya.ruch@gmail.com>
2021-02-26 20:17:24 +0300

    Update 2021-02-26T17:17:10.891Z

--- pages/egger/js/main.js
+++ pages/egger/js/main.js
@@ -376,3 +376,14 @@ function closeAllSelect(elmnt) {
 /* If the user clicks anywhere outside the select box,
 then close all select boxes: */
 document.addEventListener("click", closeAllSelect);
+
+
+
+function resetHeight(){
+    // reset the body height to that of the inner browser
+    document.body.style.height = window.innerHeight + "px";
+}
+// reset the height whenever the window's resized
+window.addEventListener("resize", resetHeight);
+// called to initially set the height.
+resetHeight();
